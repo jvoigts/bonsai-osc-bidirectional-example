901542b5704e2297039deef0331649cd9e7c4dbe
Jakob Voigts <jvoigts@gmail.com>
2022-12-03 16:17:22 -0500

    Initial commit
    minimal-ish working example

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # bonsai-osc-bidirectional-example
 Sending data back and forth between bonsai and a pyqt python program via Osc
+
+
+This is a minimal pretty unoptimized example of how to do this. The only nontrivial things are how to set up the required servers/clients on both sides, and how to throw the server on the python side in its own thread.
+
+There are likely a few things wrong with this - this is just the first working code I manged to put together with no clean up or optimization.
+
+Requires pyqt6, and python-osc ( https://python-osc.readthedocs.io/en/latest/index.html ).
+There's likely a few other requirements but this is pretty minimal.
+
+Relevant discussions are also here: https://github.com/bonsai-rx/bonsai/discussions/872
+
+ALso see Spencer Blackwood's solution: https://github.com/spencerblackwood/bonsai-osc-jupyter-example
---
+++ python/gui.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Dec.3 2022
+@author: jvoigts
+"""
+
+
+
+import sys
+
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
+from mainwindow import Ui_MainWindow
+
+import random
+
+import struct
+import numpy as np
+from numpy.random import randn
+
+
+import sys
+import time
+import datetime
+
+
+# osc stuff to get tracking data from bonsai
+from pythonosc.dispatcher import Dispatcher
+from pythonosc.osc_server import BlockingOSCUDPServer
+from pythonosc.udp_client import SimpleUDPClient
+
+	
+# in a separate thread we'll run a blocking OSC server
+class OscInputMonitor(QObject):
+
+	OscInputSignal = pyqtSignal(tuple)
+	
+	@pyqtSlot()
+	def default_handler(sig, address, *args):
+		#print(f"DEFAULT {address}: {args}")
+		x=args[1]; # unpack Osc message
+		sig.OscInputSignal.emit( (1,x) ) #<- send to main thread
+
+	@pyqtSlot()
+	def monitor_OscServer(self):
+		dispatcher = Dispatcher()
+		#dispatcher.map("/something/*", self.default_handler)
+		decoratedhandler = lambda address, *args : self.default_handler(self.OscInputSignal, address, *args) #decorate function with ref to tracking signal so the thread can send stuff back
+		dispatcher.set_default_handler(decoratedhandler)
+		server = BlockingOSCUDPServer(("127.0.0.1", 1337), dispatcher)
+		server.serve_forever()  # Blocks forever
+
+
+class behavControlGui(QMainWindow):
+	def __init__(self, parent=None):
+		QWidget.__init__(self, parent)
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
+		self.initUI()
+		
+		# set up OSC server to receive messages from bonsai
+		self.OscServer  = OscInputMonitor()
+		self.thread = QThread(self)
+		self.OscServer.OscInputSignal.connect(self.OscServer_callback)
+		self.OscServer.moveToThread(self.thread)
+		self.thread.started.connect(self.OscServer.monitor_OscServer)
+		self.thread.start()
+		print("started QThread")
+
+		# set up OSC client, connect to bonsai instance
+		# to send messages back
+		self.OSCclient = SimpleUDPClient("127.0.0.1", 1338)  # Create client
+
+
+	def sendRandomToBonsai(self):
+		print("sending random number to bonsai")
+		self.OSCclient.send_message("/127.0.0.1", np.random.rand(1)[0])  
+
+
+	@pyqtSlot(tuple)
+	def OscServer_callback(self, InputData):
+		#print(InputData);
+		self.ui.label_in.setText(str(InputData[1]))
+
+	def initUI(self):    
+		self.ui.pushButton_out.clicked.connect(self.sendRandomToBonsai)
+
+
+if __name__ == "__main__":
+	app = QApplication(sys.argv)
+	myapp = behavControlGui()
+	myapp.show()
+	sys.exit(app.exec())
+
+
+
